54e7e310a0a79c25c394f5da7f22b27607c857b4
carterprince <carteraprince@gmail.com>
2023-04-01 20:10:32 -0400

    make gray black

--- config.def.h
+++ config.def.h
@@ -8,10 +8,10 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=18" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#222222";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#000000";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#222222";
+static const char col_gray4[]       = "#000000";
 static const char col_cyan[]        = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
